9edbde7dbf75ca78502daf36acf004ef7d720c2b
krizttian <develop.km14@gmail.com>
2017-12-06 19:28:23 +0200

    DATABASES Update 06/12

---
+++ updates/06_12_2017__004.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.6.37-log : Database - blizzcms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `fx_shop_cart` */
+
+DROP TABLE IF EXISTS `fx_shop_cart`;
+
+CREATE TABLE `fx_shop_cart` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `type` int(10) NOT NULL DEFAULT '0',
+  `accountid` int(11) NOT NULL,
+  `itemid` int(11) NOT NULL,
+  `price` int(10) DEFAULT NULL,
+  `mode` varchar(2) NOT NULL,
+  `queryorcommand` text CHARACTER SET utf8,
+  `icon` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ updates/06_12_2017__005.sql
@@ -0,0 +1,28 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.6.37-log : Database - blizzcms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `fx_shop_complete` */
+
+DROP TABLE IF EXISTS `fx_shop_complete`;
+
+CREATE TABLE `fx_shop_complete` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `userid` int(10) NOT NULL,
+  `item` int(10) NOT NULL,
+  `date` varchar(100) NOT NULL,
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
