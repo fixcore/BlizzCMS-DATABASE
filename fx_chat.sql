ad50593331baceea6a142acd72302dca687d3c19
DZywolf <luis.wolf.core@gmail.com>
2018-02-21 14:45:07 -0300

    Update SQL

--- fx_chat.sql
+++ fx_chat.sql
@@ -12,18 +12,21 @@ MySQL - 5.7.17-log : Database - blizzcms
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `fx_changelogs` */
+/*Table structure for table `fx_chat` */
 
-DROP TABLE IF EXISTS `fx_changelogs`;
+DROP TABLE IF EXISTS `fx_chat`;
 
-CREATE TABLE `fx_changelogs` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  `date` int(10) NOT NULL,
-  `image` text NOT NULL,
+CREATE TABLE `fx_chat` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `from` varchar(255) NOT NULL DEFAULT '',
+  `to` varchar(255) NOT NULL DEFAULT '',
+  `message` text NOT NULL,
+  `sent` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `recd` int(10) unsigned NOT NULL DEFAULT '0',
+  `from_name` varchar(100) NOT NULL,
+  `to_name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
